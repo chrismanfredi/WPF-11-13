2721160cb6d39d103f6c97b5819ee9de7b49dde6
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-11 14:21:46 -0500

    changed the 4th prompt
    added the

--- Expressions/Expressions Assignment/Script.js
+++ Expressions/Expressions Assignment/Script.js
@@ -2,7 +2,7 @@ var FoodShopping = [];
 FoodShopping[0] = prompt ("Enter the cost of the first food item");
 FoodShopping[1] = prompt ("Enter the cost of the second food item");
 FoodShopping[2] = prompt ("Enter the cost of the second food item");
-FoodShopping[3] = prompt ("Enter cost of the second food item");
+FoodShopping[3] = prompt ("Enter the cost of the second food item");
 
 var spendinglimit = prompt("How much money can you spend on food?");
 
