05ae5418b9ebfd43cd136e43b79775ea817fe51a
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-11 13:56:39 -0500

    Expressions WS

---
+++ Expressions/ExpressionWS.js
@@ -0,0 +1,44 @@
+// Dog Years
+
+
+Age = prompt('How old is Sparky?');
+
+var Age = 5;
+var DogAge = Age * 7;
+console.log("Sparky the dog is" + Age + "human years old which is" + DogAge + "in dog years");
+
+// Slices of Pie Part 1
+
+var slices;
+var people;
+var pizzasordered;
+var pizzaperperson;
+
+slices = prompt("How many slices of pizza are in each pie?");
+people = prompt("How many people are at the party?");
+pizzasordered = prompt("How many pizza's were ordered?");
+pizzaperperson = slices * pizzasordered / people;
+
+console.log("Each person ate" + slices + "of pizza at the party");
+
+// Slices of Pie Part 2
+
+var pizzaperperson = slices * pizzasordered / people;
+
+console.log("Sparky got" + pizzaperperson + "slices of pizza");
+
+//Average shopping bill
+
+var weektotals =['102','100','154','95','102'];
+
+grocerytotals = weektotals[1] + weektotals[2] + weektotals[3] + weektotals[4] + weektotals[5];
+groceryaverage = weektotals / 5;
+
+console.log("You have spent a total of" + grocerytotals + "on groceries over 5 weeks. That's is an average of" + groceryaverage + "per week");
+
+
+
+
+
+
+
