0e407987cd46c41d663ad899088b5247bff13dcf
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-11 14:20:52 -0500

    changed the 2nd prompt
    added the

--- Expressions/Expressions Assignment/Script.js
+++ Expressions/Expressions Assignment/Script.js
@@ -1,6 +1,6 @@
 var FoodShopping = [];
 FoodShopping[0] = prompt ("Enter the cost of the first food item");
-FoodShopping[1] = prompt ("Enter cost of the second food item");
+FoodShopping[1] = prompt ("Enter the cost of the second food item");
 FoodShopping[2] = prompt ("Enter cost of the second food item");
 FoodShopping[3] = prompt ("Enter cost of the second food item");
 
