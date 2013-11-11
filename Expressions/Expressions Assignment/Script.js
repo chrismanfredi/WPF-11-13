e4f701e00132761fc21cf282e61d44cd0e6becae
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-11 14:09:11 -0500

    Spending Limit
    How much money can you spend on food

--- Expressions/Expressions Assignment/Script.js
+++ Expressions/Expressions Assignment/Script.js
@@ -4,5 +4,10 @@ FoodShopping[1] = prompt ("Enter cost of the second food item");
 FoodShopping[2] = prompt ("Enter cost of the second food item");
 FoodShopping[3] = prompt ("Enter cost of the second food item");
 
+var spendinglimit = prompt("How much money can you spend on food?");
+
+
+
+
 
 
