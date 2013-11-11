4a8ccda4c0c11347fcc3b4a12662500da5ede41c
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-11 15:42:05 -0500

    Changes
    Changes

--- Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
+++ Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
@@ -1,6 +1,26 @@
-var name;
-name = prompt('What is your name');
-var age = 7;
-age = prompt('What is your age');
-console.log("Hi my name is " + name + " and i am " + age + " years old ");
+var FoodShoppingarray = [];
+FoodShoppingarray[0] = prompt ("Enter the cost of the first food item");
+FoodShoppingarray[1] = prompt ("Enter the cost of the second food item");
+FoodShoppingarray[2] = prompt ("Enter the cost of the second food item");
+FoodShoppingarray[3] = prompt ("Enter the cost of the second food item");
+
+var spendinglimit = prompt("How much money can you spend on food?");
+
+var total = FoodShoppingarray[0] + FoodShoppingarray[1] + FoodShoppingarray[2] + FoodShoppingarray[3];
+
+if (total < spendinglimit){
+    console.log("you can buy the food!")
+
+}
+
+else{
+    console.log("You can NOT buy the food!")
+
+}
+
+
+
+
+
+
 
