665b3cb5c1972977210265806ccb3646a546fff9
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-11 15:39:05 -0500

    edit
    edit

--- Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
+++ Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
@@ -1,5 +1,5 @@
 var name;
-name = prompt('What is your name'); //string example
+name = prompt('What is your name');
 var age = 7;
 age = prompt('What is your age');
 console.log("Hi my name is " + name + " and i am " + age + " years old ");
