378699aa99967a3b5fd4e194289fc58cf5e8c4b0
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-13 14:16:52 -0500

    Comments on industry
    comments

--- Expressions/Expressions Assignment/Wacky/Expressions Assignment Wacky/Script.js
+++ Expressions/Expressions Assignment/Wacky/Expressions Assignment Wacky/Script.js
@@ -1,6 +1,6 @@
 var distance = prompt("How many miles will you be traveling?"); // Prompting the user to enter how many miles they will be traveling
 var speed = prompt("What speed will you be driving at?"); // Prompting the user to enter what speed they will be driving
-var time = speed * distance / 60;
+var time = speed * distance / 60; // Time = The speed the user is driving at Times the distance the user is traveling Divided by 60
 
 
 console.log("It will take you " + time + " Hour(s) to arrive");
