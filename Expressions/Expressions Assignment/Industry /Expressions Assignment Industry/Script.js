647033b120f73b3932f430c7347b7290899aff67
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-13 14:25:40 -0500

    comment commit 5
    commit 5

--- Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
+++ Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
@@ -2,7 +2,7 @@ var FoodShoppingarray = []; // Created the variable FoodShoppingarray
 FoodShoppingarray[0] = prompt ("Enter the cost of the first food item"); // Prompting the user to "Enter the cost of the first food item"
 FoodShoppingarray[1] = prompt ("Enter the cost of the second food item"); // Prompting the user to "Enter the cost of the second food item"
 FoodShoppingarray[2] = prompt ("Enter the cost of the third food item"); // Prompting the user to "Enter the cost of the third food item"
-FoodShoppingarray[3] = prompt ("Enter the cost of the forth food item");
+FoodShoppingarray[3] = prompt ("Enter the cost of the forth food item"); // Prompting the user to "Enter the cost of the fourth food item"
 
 var spendinglimit = prompt("How much money can you spend on food?");
 
