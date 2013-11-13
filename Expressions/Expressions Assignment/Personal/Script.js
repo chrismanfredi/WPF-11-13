34e105c64fffafa08642029bdf2cab487747c89b
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-13 14:44:20 -0500

    comment commit 4
    commit 4

--- Expressions/Expressions Assignment/Personal/Script.js
+++ Expressions/Expressions Assignment/Personal/Script.js
@@ -1,5 +1,5 @@
 var name; // Creating variable name
 name = prompt('What is your name'); // Prompting the user to type in there name
 var age; // Creating variable age
-age = prompt('What is your age');
+age = prompt('What is your age'); // Prompting the user to type in there age
 console.log("Hi my name is " + name + " and i am " + age + " years old ");
