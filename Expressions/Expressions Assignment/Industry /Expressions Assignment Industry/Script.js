ea062da93b8b3cf8e7d5447b73c1d2b14787d341
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-13 14:26:16 -0500

    comment commit 6
    commit 6

--- Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
+++ Expressions/Expressions Assignment/Industry /Expressions Assignment Industry/Script.js
@@ -4,7 +4,7 @@ FoodShoppingarray[1] = prompt ("Enter the cost of the second food item"); // Pro
 FoodShoppingarray[2] = prompt ("Enter the cost of the third food item"); // Prompting the user to "Enter the cost of the third food item"
 FoodShoppingarray[3] = prompt ("Enter the cost of the forth food item"); // Prompting the user to "Enter the cost of the fourth food item"
 
-var spendinglimit = prompt("How much money can you spend on food?");
+var spendinglimit = prompt("How much money can you spend on food?"); // Prompting the user on how much they can spend on food
 
 var total = FoodShoppingarray[0] + FoodShoppingarray[1] + FoodShoppingarray[2] + FoodShoppingarray[3];
 
