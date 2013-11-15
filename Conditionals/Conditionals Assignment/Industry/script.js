b6c15265fb058a246e1258f9654fe1f1f7251ee8
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-15 15:17:10 -0500

    more comments
    comments!

--- Conditionals/Conditionals Assignment/Industry/script.js
+++ Conditionals/Conditionals Assignment/Industry/script.js
@@ -5,17 +5,14 @@ var weight = prompt("What is your weight?"); // prompting the user there weight
 
 if(age <= 18){ // if you are under the age of 18 you are not allowed!
 
-    num ='You are under age!'
-}
-if (age >=18){
+    num ='You are under age!'} // the var num is presenting you are under age!
+if (age >=18){ // If you are above the age of 18 you are allowed
 
-    num ='You are old enough!'
-}
+    num ='You are old enough!'} // the var num is presenting you are old enough!
 
-if (weight <= 200){
+if (weight <= 200){ // if you are under the weight of 200 pounds you are allowed!
 
-    lbs = 'You are a good weight'
-}
+    lbs = 'You are a good weight'}
 if (weight >= 200){
 
     lbs = 'You are too heavy, Hit The Gym!'
