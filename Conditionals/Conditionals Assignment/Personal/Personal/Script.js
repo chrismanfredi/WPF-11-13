c37951e2706b04da313e761717c2484b974d4bf9
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-15 16:01:30 -0500

    no answer
    no answer

--- Conditionals/Conditionals Assignment/Personal/Personal/Script.js
+++ Conditionals/Conditionals Assignment/Personal/Personal/Script.js
@@ -3,7 +3,7 @@
 myjob = "web designer"; // This is the answer to the problem
 var job = prompt("What is my job"); // prompt what is my job
 
-if (job ==null || job ==""){
+if (job ==null || job ==""){ // if the user doesnt put in a answer a box will pop up
 
    alert("Alteast put something!");
 }
