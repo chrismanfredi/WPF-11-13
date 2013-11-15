daf568e63a06fd4a7dac8a43f5bfca976b03d8e7
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-15 14:58:48 -0500

    update
    update

---
+++ Conditionals/Conditionals Assignment/Personal/Personal/Script.js
@@ -0,0 +1,9 @@
+// Conditionals personal
+
+
+
+
+
+
+
+
--- Conditionals/Conditionals Assignment/Wacky/Script.js
+++ Conditionals/Conditionals Assignment/Wacky/Script.js
@@ -1,1 +1,26 @@
-// Conditional Wacky
+//Conditional Wacky
+
+//Car Transporter
+
+var maxweight = 20000;
+var bmw = prompt ("BMW Weight Ex. 3285");
+var volvo = prompt ("Volov Weight Ex. 3345");
+var lexus = prompt ("Lexus Weight Ex. 3456");
+var lamborghini = prompt ("Lamborghini Weight Ex. 3476");
+
+var total = bmw+volvo+lexus+lamborghini;
+
+if (total > maxweight){
+    //
+   console.log("Your cars are too heavy for the truck!");
+}
+else{
+    console.log("Your cars can fit on the truck");
+
+}
+
+console.log(total);
+
+
+
+
