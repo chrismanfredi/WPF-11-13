bcb741fe2a2c42143a229e5a954d2bb335b0a935
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-15 15:29:36 -0500

    conditionals wacky comments 5
    comments 5

--- Conditionals/Conditionals Assignment/Wacky/Script.js
+++ Conditionals/Conditionals Assignment/Wacky/Script.js
@@ -6,7 +6,7 @@ var maxweight = 20000; // variable max weight is set to 20,000
 var bmw = prompt ("BMW Weight Ex. 3285"); // prompting the user to enter how much the bmw weighs
 var volvo = prompt ("Volov Weight Ex. 3345"); // prompting the user to enter how much the volvo weighs
 var lexus = prompt ("Lexus Weight Ex. 3456"); // prompting the user to enter how much the lexus weighs
-var lamborghini = prompt ("Lamborghini Weight Ex. 3476");
+var lamborghini = prompt ("Lamborghini Weight Ex. 3476"); // prompting the user to enter how much the lamborghini weighs
 
 var total = bmw+volvo+lexus+lamborghini;
 
