527b44298ae218b3a55a2a71341865ff1d786621
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-18 11:24:16 -0500

    typo
    typo

--- Functions/Functions Assingment/Personal/script.js
+++ Functions/Functions Assingment/Personal/script.js
@@ -3,7 +3,7 @@
 var assignment1 = parseInt(prompt("Assignment 1 grade")); // create var assignment 1 and prompting the user to enter the grade
 var assignment2 = parseInt(prompt("Assignment 2 grade")); // create var assignment 2 and prompting the user to enter the grade
 var assignment3 = parseInt(prompt("Assignment 3 grade")); // create var assignment 3 and prompting the user to enter the grade
-var assignment4 = parseInt(prompt("Assignment 4 grade")); // create var assignment 3 and prompting the user to enter the grade
+var assignment4 = parseInt(prompt("Assignment 4 grade")); // create var assignment 4 and prompting the user to enter the grade
 
 function gradeaverage(){ // function grade average
     var gradetotals=assignment1+assignment2+assignment3+assignment4; // variable grade totals and adding all the assignment grades
