c2840891af4b5fcdea813fe02fe667bf2d166f95
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 09:36:04 -0500

    Revert "condtionals wacky comments 3"
    This reverts commit 1cad041fd976616c2d936c0edb3f7ed50e77d9aa.

--- Conditionals/Conditionals Assignment/Wacky/Script.js
+++ Conditionals/Conditionals Assignment/Wacky/Script.js
@@ -5,7 +5,7 @@
 var maxweight = 20000; // variable max weight is set to 20,000
 var bmw = prompt ("BMW Weight Ex. 3285"); // prompting the user to enter how much the bmw weighs
 var volvo = prompt ("Volov Weight Ex. 3345"); // prompting the user to enter how much the volvo weighs
-var lexus = prompt ("Lexus Weight Ex. 3456"); // prompting the user to enter how much the lexus weighs
+var lexus = prompt ("Lexus Weight Ex. 3456");
 var lamborghini = prompt ("Lamborghini Weight Ex. 3476");
 
 var total = bmw+volvo+lexus+lamborghini;
