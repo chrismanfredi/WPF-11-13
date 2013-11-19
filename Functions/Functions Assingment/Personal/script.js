0f405900cf12653db2eb5ffc7c5515f968e535fc
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 09:36:03 -0500

    Revert "created function gradeaverage"
    This reverts commit ba77a101d686a3f1a44bcc49e356fabd6dbc99b6.

--- Functions/Functions Assingment/Personal/script.js
+++ Functions/Functions Assingment/Personal/script.js
@@ -5,7 +5,3 @@ var assignment2 = parseInt(prompt("Assignment 2 grade"));
 var assignment3 = parseInt(prompt("Assignment 3 grade"));
 var assignment4 = parseInt(prompt("Assignment 4 grade"));
 
-function gradeaverage(){
-
-}
-
