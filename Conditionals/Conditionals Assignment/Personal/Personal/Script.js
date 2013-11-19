74cdaa89c6160b946d42f9f7d472c52c808c787e
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 09:36:04 -0500

    Revert "no answer"
    This reverts commit c37951e2706b04da313e761717c2484b974d4bf9.

--- Conditionals/Conditionals Assignment/Personal/Personal/Script.js
+++ Conditionals/Conditionals Assignment/Personal/Personal/Script.js
@@ -3,7 +3,7 @@
 myjob = "web designer"; // This is the answer to the problem
 var job = prompt("What is my job"); // prompt what is my job
 
-if (job ==null || job ==""){ // if the user doesnt put in a answer a box will pop up
+if (job ==null || job ==""){
 
    alert("Alteast put something!");
 }
