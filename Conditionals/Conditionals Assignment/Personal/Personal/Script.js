72d1136e5a2ff5db24bf8249d3b10336e35311b6
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 09:36:03 -0500

    Revert "conditianals indusrty2"
    This reverts commit ac5aa990298b41c237700c83e010c334d5de9cf8.

--- Conditionals/Conditionals Assignment/Personal/Personal/Script.js
+++ Conditionals/Conditionals Assignment/Personal/Personal/Script.js
@@ -1,4 +1,21 @@
+// Conditionals personal
 
+// Can you guess my job?
+
+myjob = "web designer"; // This is the answer to the problem
+var job = prompt("What is my job"); // prompt what is my job
+
+if (job ==null || job ==""){ // if the user doesnt put in a answer a box will pop up
+
+   alert("Alteast put something!"); // Alert pop up box
+}
+else if (job == myjob) // if the answer is right the user will put web designer
+{
+      console.log("You guessed my job!"); // console log you guessed my job!
+}
+else {
+    console.log("WRONGGGG!"); // console long WRONGGGG
+}
 
 
 
