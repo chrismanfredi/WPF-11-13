88350642f61f79ba1dae4a2e86cb744fb2ea1258
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 10:08:01 -0500

    commit
    commit

--- Conditionals/Conditionals Assignment/Wacky/Script.js
+++ Conditionals/Conditionals Assignment/Wacky/Script.js
@@ -2,24 +2,24 @@
 
 //Car Transporter
 
-var maxweight = 20000;
-var bmw = prompt ("BMW Weight Ex. 3285");
-var volvo = prompt ("Volov Weight Ex. 3345");
-var lexus = prompt ("Lexus Weight Ex. 3456");
-var lamborghini = prompt ("Lamborghini Weight Ex. 3476");
+var maxweight = 20000; // max weight is set to 20000
+var bmw = prompt ("BMW Weight Ex. 3285"); // prompting the user to enter the BMW weight
+var volvo = prompt ("Volvo Weight Ex. 3345"); // prompting the user to enter the Volvo weight
+var lexus = prompt ("Lexus Weight Ex. 3456"); // prompting the user to enter the Lexus weight
+var lamborghini = prompt ("Lamborghini Weight Ex. 3476"); // prompting the user to end the lamborghini weight
 
-var total = bmw+volvo+lexus+lamborghini;
+var total = bmw+volvo+lexus+lamborghini; // Math of the problem, adding all the car weights
 
-if (total > maxweight){
+if (total > maxweight){ // if the total weight of the cars is more then 20000
     //
    console.log("Your cars are too heavy for the truck!");
 }
-else{
+else{ // or if the cars aren't too heavy
     console.log("Your cars can fit on the truck");
 
 }
 
-console.log(total);
+console.log(total); // console log total weight
 
 
 
