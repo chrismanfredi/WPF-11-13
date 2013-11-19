e26997db7677f15f3d7790e1fba598079b8870f6
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 09:36:03 -0500

    Revert "edit conditionals"
    This reverts commit 361ab718851665ffbc6bcd0d995bbf7cfd6fbd36.

--- Conditionals/Conditionals Assignment/Industry/script.js
+++ Conditionals/Conditionals Assignment/Industry/script.js
@@ -1,4 +1,17 @@
+myjob = "web designer"; // Plugging in the answer to my favorite color
+var job = prompt("What is my job?"); // Asking user to guess a color
 
+if (job ==null || job =="") // If user inputs nothing a message pops up
+{
+    alert("Atleast put something!"); // This is the message the user will see if they don't input anything
+}
+else if (job === myjob) // If the color guessed equals to my favorite color
+{
+    console.log("You guess my job!"); // Message sent if answered correctly
+}
+else {
+    console.log("WRONGGGG!"); // Message sent if answered wrongly
+}
 
 
 
--- Functions/Functions Assingment/Personal/script.js
+++ Functions/Functions Assingment/Personal/script.js
@@ -2,44 +2,15 @@
 
 // grade average calculator
 
-function gradeaverage(){ // function grade average
-    var gradetotals=assignment1+assignment2+assignment3+assignment4; // variable grade totals and adding all the assignment grades
-    return gradetotals/4
-} // returning what the user put in for grade totals divided by 4 to find the average
-
-
-
 var assignment1 = parseInt(prompt("Assignment 1 grade")); // create var assignment 1 and prompting the user to enter the grade
 var assignment2 = parseInt(prompt("Assignment 2 grade")); // create var assignment 2 and prompting the user to enter the grade
 var assignment3 = parseInt(prompt("Assignment 3 grade")); // create var assignment 3 and prompting the user to enter the grade
 var assignment4 = parseInt(prompt("Assignment 4 grade")); // create var assignment 4 and prompting the user to enter the grade
 
-
-
-
-if (gradeaverage() < 70){
-
-    x = 'You fail'
-}
-  else {x = 'You Pass'}
-
-
+function gradeaverage(){ // function grade average
+    var gradetotals=assignment1+assignment2+assignment3+assignment4; // variable grade totals and adding all the assignment grades
+    return gradetotals/4
+} // returning what the user put in for grade totals divided by 4 to find the average
 
 console.log(gradeaverage()); // console log grade average
-console.log(x);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
