531cee84b58be286ea54693b3a071848e7364b0b
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-19 09:36:03 -0500

    Revert "grade average calculator"
    This reverts commit 300ef36f801830a099aa8753a1b9899f38f7c778.

--- Functions/Functions Assingment/Personal/script.js
+++ Functions/Functions Assingment/Personal/script.js
@@ -1,7 +1,5 @@
 // Functions Personal
 
-// grade average calculator
-
 var assignment1 = parseInt(prompt("Assignment 1 grade")); // create var assignment 1 and prompting the user to enter the grade
 var assignment2 = parseInt(prompt("Assignment 2 grade")); // create var assignment 2 and prompting the user to enter the grade
 var assignment3 = parseInt(prompt("Assignment 3 grade")); // create var assignment 3 and prompting the user to enter the grade
