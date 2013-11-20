6d0ed7ee55f8ce826797505420c4bd022ad0b74d
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-20 15:10:18 -0500

    functions wacky
    wacky

--- Functions/Functions Assingment/Wacky/script.js
+++ Functions/Functions Assingment/Wacky/script.js
@@ -4,7 +4,7 @@ var year= prompt('Please enter the year you were born');
 var currentyear= 2013;
 
 function agecheck(){
-    var age = curentyear-year;
+    var age = currentyear-year;
 
     if(year==''){
         return 'Please enter the year you were born!'
@@ -12,8 +12,8 @@ function agecheck(){
         return 'Please enter a year that is not in the future'
     }else if(age<18){
         return 'your current age is '+age+', Leave Now! the club is not for you'
+    }else{
+        return 'your current age is '+age+',Come right in! and have a good time'
     }
-
-
-}
+} console.log(agecheck());
 
