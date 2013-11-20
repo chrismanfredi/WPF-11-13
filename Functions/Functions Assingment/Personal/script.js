06d8fae79a256f2f66990cac11996f336ba73407
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-20 09:37:30 -0500

    final comments functions personal
    comments

--- Functions/Functions Assingment/Personal/script.js
+++ Functions/Functions Assingment/Personal/script.js
@@ -17,19 +17,19 @@ var test2 = parseInt(prompt('Assignment grade 2')); // prompting the user to ent
 var test3 = parseInt(prompt('Assignment grade 3')); // prompting the user to enter assignment grade 3
 var test4 = parseInt(prompt('Assignment grade 4')); // prompting the user to enter assignment grade 4
 
-var added = gradeAverage(test1, test2, test3, test4);
+var added = gradeAverage(test1, test2, test3, test4); // math of the problem adding all the test grades
 
-var testAmt = 4;
-var wholeaverage = divide(added, testAmt);
+var testAmt = 4; // creating variable test Amt to divide the test scores by 4
+var wholeaverage = divide(added, testAmt); // creating variable whole average which divides the added variable and the test scores
 
-if (wholeaverage < 70){
+if (wholeaverage < 70){ // if the whole average is less then 70
 
-    x = "You fail!"
+    x = "You fail!" // you fail!
 }
-else {x="you pass!"}
+else {x="you pass!"} // or you pass!
 
-console.log(wholeaverage);
-console.log(x);
+console.log(wholeaverage); // console log "whole average"
+console.log(x); // console log "x"
 
 
 
