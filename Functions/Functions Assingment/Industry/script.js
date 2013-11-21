fd62216c5d8a6b3d19aa6c177ebda4d5eb84d0a9
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-21 09:50:44 -0500

    functions industry
    industry

--- Functions/Functions Assingment/Industry/script.js
+++ Functions/Functions Assingment/Industry/script.js
@@ -1,6 +1,6 @@
 function grass(a,b,c){
 
-    var grass1 = a * b * c;
+    var grass1 = a + b +c;
     return grass1;
 }
 
@@ -13,16 +13,18 @@ function landscape(x,y){
 }
 
 
-var perhouse= prompt("Enter how much you make per house");
-var week1= prompt("Please enter how many hours you worked week 1");
-var week2= prompt("Please enter how many hours you worked week 2");
-var week3= prompt("Please enter how many hours you worked week 3");
+var perhouse= parseInt(prompt('Enter how much you make per house'));
+var week1= parseInt(prompt('Please enter how many hours you worked week 1'));
+var week2= parseInt(prompt('Please enter how many hours you worked week 2'));
+var week3= parseInt(prompt('Please enter how many hours you worked week 3'));
 
 var grasscall = grass(week1, week2, week3);
 
 var grassfinal = landscape(grasscall, perhouse);
 
 console.log(grassfinal);
+
+console.log("Over the 3 weeks you worked a total of " + grasscall + " hours, and made a total of " + grassfinal + " Dollars!");
 
 
 
