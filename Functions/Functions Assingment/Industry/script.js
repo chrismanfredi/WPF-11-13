8485772d732cd966941b10d7e7b355e01ecec6c4
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-11-21 09:56:43 -0500

    comments
    comments

--- Functions/Functions Assingment/Industry/script.js
+++ Functions/Functions Assingment/Industry/script.js
@@ -14,9 +14,9 @@ function landscape(x,y){ // created function landscape
 
 
 var perhouse= parseInt(prompt('Enter how much you make per house')); // varibale perhouse prompting the user to enter a value
-var week1= parseInt(prompt('Please enter how many hours you worked week 1'));
-var week2= parseInt(prompt('Please enter how many hours you worked week 2'));
-var week3= parseInt(prompt('Please enter how many hours you worked week 3'));
+var week1= parseInt(prompt('Please enter how many hours you worked week 1')); // variable week1 prompting the user to enter how much they worked
+var week2= parseInt(prompt('Please enter how many hours you worked week 2')); // variable week2 prompting the user to enter how much they worked
+var week3= parseInt(prompt('Please enter how many hours you worked week 3')); // variable week3 prompting the user to enter how much they worked
 
 var grasscall = grass(week1, week2, week3);
 
